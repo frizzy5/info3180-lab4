ee4c5d2d57601c09b9f622561697f4cab957072b
Jevoy <jevoy.charvis@mymona.uwi.edu>
2019-02-24 21:48:47 +0000

    edit4

--- app/views.py
+++ app/views.py
@@ -48,6 +48,16 @@ def upload():
     flash_errors(uploadForm)
     return render_template('upload.html', uploadForm=uploadForm)
 
+def get_uploaded_images():
+    rootdir = os.getcwd()
+    for subdir, dirs, files in os.walk(app.config['UPLOAD_FOLDER']):
+        for file in files:
+            return os.path.join(subdir, file)
+@app.route('/files')
+def files():
+    images = get_uploaded_images()
+    return render_template('files.html', images=images)
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
